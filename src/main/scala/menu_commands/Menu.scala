d024e4743ebc5165b179160111353e7f4662f03e
Mitchell DeRidder <mitchell.deridder@usma.edu>
2016-01-31 22:30:15 -0500

    v0.1.0
    DeRidder

--- src/main/scala/menu_commands/Menu.scala
+++ src/main/scala/menu_commands/Menu.scala
@@ -1,39 +1,81 @@
 package menu_commands
 
+//******* MENU *******
 object Menu {
   
   def showGameArea : String = {
-    ???
-  }
+    Board.show + "\n" +
+    Scoreboard.show + "\n" +
+    }
   
   def showPlayerOrder : String = {
-???
+    return PlayerOrder.show
   }
   
   def advancePlayerOrder : String = {
-???
+    PlayerOrder.advance
+    return PlayerOrder.show
   }
 }
 
-
-//******* BOARD *******
+//*********** BOARD ******************
 object Board {
   
-  // creates the 9 boxes that form the grid
-  ???
+  val spot = Array[String](9) //fill each board spot type as wanted
+
+  def show = {
+    var result = "Board: \n" +
+    result += "___________________________________________________________________________________________ \n"
+    for(i <- spot){
+      result += "|     " + spot(i) + "     | \n"
+    }
+    result += "___________________________________________________________________________________________ \n"
+    result
+  }
 }
+
+//******* SCOREBOARD *******
+object Scoreboard {
+  def show = {
+    var result = "Scores:\n"
+    for (p <- PlayerOrder.toArray) 
+      result += p.name + " =  Job: " + p.job + ", Money: " + p.money + ", Roll: " + p.roll + ", Stocks: " + p.stocks + ", Insurance: " + p.insurance + ", Education: " + p.education + "\n"
+    result.substring(0, result.length-2) + "\n"         
+  }
+    
+}
+
 
 
 
 //******* PLAYER_ORDER *******
-object PlayerOrder{ //extends scala.collection.mutable.Queue[Player] {
+object PlayerOrder extends scala.collection.mutable.Queue[Player] {
   
-  ???
+  this += new Player("Car 1")
+  this += new Player("Car 2")
+  this += new Player("Car 3")
+  this += new Player("Car 4")
+  
+  def advance {
+    this += this.dequeue
+  }
+  
+  def show : String = {
+    var result = ""    
+    for (p <- this.toArray) 
+      result += p.name + ", "
+    result.substring(0, result.length-2)
+  }
   
 }
 
 
 //******* PLAYER *******
-class Player (val name : String) {//creates HUD
-  ???
+class Player (val name : String) {
+  var job = "None"
+  var money = "$10000"
+  var roll = "None"
+  var stocks = "No"
+  var insurance = ArrayBuffer[String]("")
+  var education = "None"
 }
